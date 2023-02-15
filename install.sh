603d744bf47ea2252e1dba63bba66444001bc1c1
fozba <45905263+fozba@users.noreply.github.com>
2023-02-15 03:24:52 +0000

    Updated.

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-script_dir=$(dirname "$(readlink -f "$0")")
-
-rm -rf $HOME/.bash_profile
-rm -rf $HOME/.bashrc
-
-ln -s $script_dir/bash_profile $HOME/.bash_profile
-ln -s $script_dir/bashrc $HOME/.bashrc
